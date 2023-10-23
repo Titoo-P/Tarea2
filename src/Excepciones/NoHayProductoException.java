0bd11dd25bb182eae20974b06ee02a0d13885718
panto <pantostado1601@gmail.com>
2023-10-23 06:57:51 -0300

    Commit 3: [Excepcciones](verificar si funciona en package)

---
+++ src/Excepciones/NoHayProductoException.java
@@ -0,0 +1,8 @@
+package Excepciones;
+
+public class NoHayProductoException extends Exception {
+
+    public NoHayProductoException(String excepcion) {
+        super(excepcion); //mensaje en las classes
+    }
+}
---
+++ src/Excepciones/PagoIncorrectoException.java
@@ -0,0 +1,8 @@
+package Excepciones;
+
+public class PagoIncorrectoException extends Exception {
+
+    public PagoIncorrectoException(String excepcion) {
+        super(excepcion); // se le ingresa el mensaje en las classes
+    }
+}
